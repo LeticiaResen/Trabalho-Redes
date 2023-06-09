b6d34522bd43fa55b72e3ff453da8776d72f1cbd
LeticiaResend <leticiaresende.estudo@gmail.com>
2023-06-09 19:13:16 -0300

    Inicio desconexão

--- Equipament.c
+++ Equipament.c
@@ -46,6 +46,10 @@ int main(int argc, char *argv[])
 		perror("connection failed");
 		exit(EXIT_FAILURE);
 	}
+
+	fd_set readfds;
+	int max_sd;
+
 	// Enviando o REQ_ADD
 	memset(buffer, 0, 256);
 	buffer[0] = 5; // Tipo da mensagem (Id Msg) - REQ_ADD
@@ -55,84 +59,113 @@ int main(int argc, char *argv[])
 		printf("ERROR writing to socket");
 	}
 	printf("REQ_ADD sent\n");
+
 	while (1)
 	{
-		// Clear the buffer
-		memset(buffer, 0, BUFFER_SIZE);
+		FD_ZERO(&readfds);
+		FD_SET(STDIN_FILENO, &readfds);
+		FD_SET(sock, &readfds);
+		max_sd = (sock > STDIN_FILENO) ? sock : STDIN_FILENO;
 
-		// // Wait for user input
-		// printf("Enter a message: ");
-		// fgets(buffer, BUFFER_SIZE, stdin);
-
-		// // Send message to the server
-		// send(sock, buffer, strlen(buffer), 0);
-
-		// Receive the response from the server
-		memset(buffer, 0, BUFFER_SIZE);
-		recv(sock, buffer, BUFFER_SIZE, 0);
-
-		// Verify the type of the message
-		int msg_type = buffer[0];
-
-		if (msg_type == 11)
+		// Wait for activity on any of the file descriptors
+		if (select(max_sd + 1, &readfds, NULL, NULL, NULL) < 0)
 		{
-			// Caso for um erro, pegar o código do erro
-			int code = buffer[1];
-			// Imprimir a mensagem de acordo com o código
-			switch (code)
-			{
-			case 1:
-				printf("Error: Equipament not found.\n");
-				break;
-			case 2:
-				printf("Error: Source equipament not found.\n");
-				break;
-			case 3:
-				printf("Error: Target equipament not found.\n");
-				break;
-			case 4:
-				printf("Error: Equipament limit exceeded.\n");
-				break;
-			case 6:
-				printf("Error: Peer limit exceed.\n");
-				break;
-			case 7:
-				printf("Error: Peer not found.\n");
-				break;
-			}
+			perror("select error");
+			exit(EXIT_FAILURE);
 		}
-		if (msg_type == 7) // It's a RES_ADD
+
+		// Check if there is a message from the server
+		if (FD_ISSET(sock, &readfds))
 		{
-			// Find Out the number of the new equipament
-			int eq_code = buffer[1];
-			// Print the message
-			if (number_equipament == 0)
+			// Clear the buffer
+			memset(buffer, 0, BUFFER_SIZE);
+
+			// Receive the response from the server
+			recv(sock, buffer, BUFFER_SIZE, 0);
+
+			// Verify the type of the message
+			int msg_type = buffer[0];
+
+			if (msg_type == 11)
 			{
-				equipament_ids[0] = eq_code;
-				printf("New ID: %d \n", eq_code);
-			}
-			else
-			{
-				equipament_ids[number_equipament++] = eq_code;
-				printf("Equipament IdEq %d added\n", eq_code);
-			}
-		}
-		if (msg_type == 8){ // It's a RES_LIST
-			for (int i = 1; i < 16; i++)
-			{
-				if (buffer[i]!=0)
+				// Caso for um erro, pegar o código do erro
+				int code = buffer[1];
+				// Imprimir a mensagem de acordo com o código
+				switch (code)
 				{
-					//printf("Numero a ser salvo %d\n", buffer[i]);
-					equipament_ids[i]= buffer[i];
-					number_equipament++;
+				case 1:
+					printf("Error: Equipment not found.\n");
+					break;
+				case 2:
+					printf("Error: Source equipment not found.\n");
+					break;
+				case 3:
+					printf("Error: Target equipment not found.\n");
+					break;
+				case 4:
+					printf("Error: Equipment limit exceeded.\n");
+					break;
+				case 6:
+					printf("Error: Peer limit exceeded.\n");
+					break;
+				case 7:
+					printf("Error: Peer not found.\n");
+					break;
 				}
 			}
-				//printf("Number equipaments %d\n",number_equipament);
-			for(int i=0; i<number_equipament; i++){
-				//printf("Recebi a lista e registrei o equipamentos %d no i: %d\n",equipament_ids[i], i);
+			if (msg_type == 7) // It's a RES_ADD
+			{
+				// Find out the number of the new equipment
+				int eq_code = buffer[1];
+				// Print the message
+				if (number_equipament == 0)
+				{
+					equipament_ids[0] = eq_code;
+					printf("New ID: %d \n", eq_code);
+				}
+				else
+				{
+					equipament_ids[number_equipament++] = eq_code;
+					printf("Equipment IdEq %d added\n", eq_code);
+				}
+			}
+			if (msg_type == 8)
+			{ // It's a RES_LIST
+				for (int i = 1; i < 16; i++)
+				{
+					if (buffer[i] != 0)
+					{
+						equipament_ids[i] = buffer[i];
+						number_equipament++;
+					}
+				}
+				for (int i = 0; i < number_equipament; i++)
+				{
+					printf("Received the list and registered equipment %d at index: %d\n", equipament_ids[i], i);
+				}
 			}
 		}
-		// printf("Server response: %s\n", buffer);
+
+		// Check if there is a message from the user
+		if (FD_ISSET(STDIN_FILENO, &readfds))
+		{
+			// Clear the buffer
+			memset(buffer, 0, BUFFER_SIZE);
+
+			// Read the message from the user
+			fgets(buffer, BUFFER_SIZE, stdin);
+
+			// Verify whats is the message
+			if (strncmp(buffer, "close connection", 16) == 0)
+			{
+				memset(buffer, 0, BUFFER_SIZE);
+				buffer[0]=6; // Id message 
+				buffer[1]=equipament_ids[0]; // number of id
+			}
+
+			// Send message to the server
+			send(sock, buffer, strlen(buffer), 0);
+		}
 	}
 
 	// Close the socket
--- Servers.c
+++ Servers.c
@@ -13,192 +13,217 @@
 int equipament_ids[MAX_CLIENTS];
 int number_equipament = 0;
 
-int main(int argc, char *argv[])
+void disconect_client(int id)
 {
-	if (argc != 2)
+	for (int i = 0; i < number_equipament; i++)
 	{
-		printf("Usage: %s <server_port>\n", argv[0]);
-		return 1;
-	}
-
-	int server_fd, client_fds[MAX_CLIENTS], max_fd, activity, i, valread, new_socket, sd;
-	int max_clients = MAX_CLIENTS;
-	int client_count = 0;
-	socklen_t addrlen;
-	struct sockaddr_in address;
-	char buffer[BUFFER_SIZE];
-
-	// Set up server socket
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(atoi(argv[1]));
-
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-
-	if (listen(server_fd, max_clients) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-
-	// Initialize client file descriptor set and set server socket as the highest file descriptor
-	fd_set readfds;
-	for (i = 0; i < max_clients; i++)
-	{
-		client_fds[i] = 0;
-	}
-	max_fd = server_fd;
-
-	while (1)
-	{
-		FD_ZERO(&readfds);
-		FD_SET(server_fd, &readfds);
-		for (i = 0; i < max_clients; i++)
+		if (equipament_ids[i] == id)
 		{
-			sd = client_fds[i];
-			if (sd > 0)
+			// Shift the remaining IDs to fill the gap
+			for (int k = i; k < number_equipament - 1; k++)
 			{
-				FD_SET(sd, &readfds);
+				equipament_ids[k] = equipament_ids[k + 1];
 			}
-			if (sd > max_fd)
-			{
-				max_fd = sd;
-			}
-		}
-
-		// Wait for an activity on one of the sockets using select
-		activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
-		if ((activity < 0) && (errno != EINTR))
-		{
-			perror("select error");
-		}
-
-		// If the server socket has a new connection, accept it
-		if (FD_ISSET(server_fd, &readfds))
-		{
-			addrlen = sizeof(address);
-			if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0)
-			{
-				perror("accept");
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				if (client_count >= max_clients)
-				{
-					printf("Maximum connections reached. Rejecting new connection.\n");
-					// Building message ERROR(04)
-					memset(buffer, 0, BUFFER_SIZE);
-					buffer[0] = 11; // Type of the message (Id Msg) - ERROR
-					buffer[1] = 4;	// Number of the error - 04
-					// Sending the error message
-					send(new_socket, buffer, 2, 0);
-					close(new_socket);
-				}
-				else
-				{
-					printf("Equipment IdEq%d added\n", new_socket);
-					printf("New connection, socket fd is %d, IP is: %s, port: %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-
-					// Add new socket to the array of client sockets
-					for (i = 0; i < max_clients; i++)
-					{
-						if (client_fds[i] == 0)
-						{
-							client_fds[i] = new_socket;
-							client_count++;
-							break;
-						}
-					}
-					// Creating the RES_ADD
-					memset(buffer, 0, BUFFER_SIZE);
-					buffer[0] = 7;			// Type of the message (Id Msg) - RES_ADD
-					buffer[1] = new_socket; // Number of the equipment
-
-					// Save the equipament ID
-					equipament_ids[number_equipament++] = new_socket;
-
-					// Broadcast the new connection
-					for (i = 0; i < max_clients; i++)
-					{
-						if (client_fds[i] != 0)
-						{
-							// Send RES_ADD
-							int n = send(client_fds[i], buffer, 2, 0);
-							if (n <= 0)
-							{
-								printf("Erro ao enviar RES_ADD");
-							}
-							else
-							{
-								printf("RES_ADD enviado com sucesso para %d \n", client_fds[i]);
-							}
-						}
-					}
-					// Send RES_LIST for the new equipament
-					memset(buffer, 0, BUFFER_SIZE);
-					buffer[0] = 8;			// Type of the message (Id Msg) - RES_LIST
-					//printf("Tenho %d equipamentos\n",number_equipament);
-					for(int i=0; i<number_equipament; i++){
-						//printf("i: %d  Number equipament: %d \n",i,equipament_ids[i]);
-						buffer[i + 1] = equipament_ids[i];
-						// printf("%d\n",buffer[i+1]);
-					}
-					send(new_socket,buffer,256,0);
-				}
-			}
-		}
-
-		// Check for IO operations on other sockets (data from clients)
-		for (i = 0; i < max_clients; i++)
-		{
-			sd = client_fds[i];
-			if (FD_ISSET(sd, &readfds))
-			{
-				if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0)
-				{
-					// Client has disconnected
-					getpeername(sd, (struct sockaddr *)&address, &addrlen);
-					printf("Host disconnected, IP: %s, Port: %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-
-					close(sd);
-					client_fds[i] = 0;
-					client_count--;
-
-					// Remove the disconnected equipament ID
-					for (int j = 0; j < number_equipament; j++)
-					{
-						if (equipament_ids[j] == sd)
-						{
-							// Shift the remaining IDs to fill the gap
-							for (int k = j; k < number_equipament - 1; k++)
-							{
-								equipament_ids[k] = equipament_ids[k + 1];
-							}
-							number_equipament--;
-							break;
-						}
-					}
-				}
-				else
-				{
-					// Echo back the received message
-					buffer[valread] = '\0';
-					send(sd, buffer, strlen(buffer), 0);
-				}
-			}
+			number_equipament--;
+			break;
 		}
 	}
-
-	return 0;
 }
+
+	int main(int argc, char *argv[])
+	{
+		if (argc != 2)
+		{
+			printf("Usage: %s <server_port>\n", argv[0]);
+			return 1;
+		}
+
+		int server_fd, client_fds[MAX_CLIENTS], max_fd, activity, i, valread, new_socket, sd;
+		int max_clients = MAX_CLIENTS;
+		int client_count = 0;
+		socklen_t addrlen;
+		struct sockaddr_in address;
+		char buffer[BUFFER_SIZE];
+
+		// Set up server socket
+		if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+		{
+			perror("socket failed");
+			exit(EXIT_FAILURE);
+		}
+
+		address.sin_family = AF_INET;
+		address.sin_addr.s_addr = INADDR_ANY;
+		address.sin_port = htons(atoi(argv[1]));
+
+		if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+		{
+			perror("bind failed");
+			exit(EXIT_FAILURE);
+		}
+
+		if (listen(server_fd, max_clients) < 0)
+		{
+			perror("listen");
+			exit(EXIT_FAILURE);
+		}
+
+		// Initialize client file descriptor set and set server socket as the highest file descriptor
+		fd_set readfds;
+		for (i = 0; i < max_clients; i++)
+		{
+			client_fds[i] = 0;
+		}
+		max_fd = server_fd;
+
+		while (1)
+		{
+			FD_ZERO(&readfds);
+			FD_SET(server_fd, &readfds);
+			for (i = 0; i < max_clients; i++)
+			{
+				sd = client_fds[i];
+				if (sd > 0)
+				{
+					FD_SET(sd, &readfds);
+				}
+				if (sd > max_fd)
+				{
+					max_fd = sd;
+				}
+			}
+
+			// Wait for an activity on one of the sockets using select
+			activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+			if ((activity < 0) && (errno != EINTR))
+			{
+				perror("select error");
+			}
+
+			// If the server socket has a new connection, accept it
+			if (FD_ISSET(server_fd, &readfds))
+			{
+				addrlen = sizeof(address);
+				if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0)
+				{
+					perror("accept");
+					exit(EXIT_FAILURE);
+				}
+				else
+				{
+					if (client_count >= max_clients)
+					{
+						printf("Maximum connections reached. Rejecting new connection.\n");
+						// Building message ERROR(04)
+						memset(buffer, 0, BUFFER_SIZE);
+						buffer[0] = 11; // Type of the message (Id Msg) - ERROR
+						buffer[1] = 4;	// Number of the error - 04
+						// Sending the error message
+						send(new_socket, buffer, 2, 0);
+						close(new_socket);
+					}
+					else
+					{
+						printf("Equipment IdEq%d added\n", new_socket);
+						printf("New connection, socket fd is %d, IP is: %s, port: %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+						// Add new socket to the array of client sockets
+						for (i = 0; i < max_clients; i++)
+						{
+							if (client_fds[i] == 0)
+							{
+								client_fds[i] = new_socket;
+								client_count++;
+								break;
+							}
+						}
+						// Creating the RES_ADD
+						memset(buffer, 0, BUFFER_SIZE);
+						buffer[0] = 7;			// Type of the message (Id Msg) - RES_ADD
+						buffer[1] = new_socket; // Number of the equipment
+
+						// Save the equipament ID
+						equipament_ids[number_equipament++] = new_socket;
+
+						// Broadcast the new connection
+						for (i = 0; i < max_clients; i++)
+						{
+							if (client_fds[i] != 0)
+							{
+								// Send RES_ADD
+								int n = send(client_fds[i], buffer, 2, 0);
+								if (n <= 0)
+								{
+									printf("Erro ao enviar RES_ADD");
+								}
+								else
+								{
+									printf("RES_ADD enviado com sucesso para %d \n", client_fds[i]);
+								}
+							}
+						}
+						// Send RES_LIST for the new equipament
+						memset(buffer, 0, BUFFER_SIZE);
+						buffer[0] = 8; // Type of the message (Id Msg) - RES_LIST
+						// printf("Tenho %d equipamentos\n",number_equipament);
+						for (int i = 0; i < number_equipament; i++)
+						{
+							// printf("i: %d  Number equipament: %d \n",i,equipament_ids[i]);
+							buffer[i + 1] = equipament_ids[i];
+							// printf("%d\n",buffer[i+1]);
+						}
+						send(new_socket, buffer, 256, 0);
+					}
+				}
+			}
+
+			// Check for IO operations on other sockets (data from clients)
+			for (i = 0; i < max_clients; i++)
+			{
+				sd = client_fds[i];
+				if (FD_ISSET(sd, &readfds))
+				{
+					if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0)
+					{
+						// Client has disconnected
+						getpeername(sd, (struct sockaddr *)&address, &addrlen);
+						printf("Host disconnected, IP: %s, Port: %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+						close(sd);
+						client_fds[i] = 0;
+						client_count--;
+
+						// Remove the disconnected equipament ID
+						for (int j = 0; j < number_equipament; j++)
+						{
+							if (equipament_ids[j] == sd)
+							{
+								// Shift the remaining IDs to fill the gap
+								for (int k = j; k < number_equipament - 1; k++)
+								{
+									equipament_ids[k] = equipament_ids[k + 1];
+								}
+								number_equipament--;
+								break;
+							}
+						}
+					}
+					else
+					{
+						// Echo back the received message
+						buffer[valread] = '\0';
+						printf("Received message from client %d: %s\n", sd, buffer);
+						if (buffer[0] == 6)
+						{
+							int num_eq = buffer[1];
+							disconect_client(num_eq);
+							printf("Equipament IdEq%d removed\n",num_eq);
+							printf("Number of clients: %d\n",number_equipament);
+						}
+					}
+				}
+			}
+		}
+
+		return 0;
+	}
